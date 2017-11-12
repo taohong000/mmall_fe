d77cba44a9e7ec39ea37b19fd081bc328673be60
taohong000 <th19891014@qq.com>
2017-11-12 23:43:44 +0800

    order

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Author: taohong
  * @Date:   2017-10-22 23:14:54
  * @Last Modified by:   taohong
- * @Last Modified time: 2017-11-10 23:00:58
+ * @Last Modified time: 2017-11-12 23:33:24
  */
 var webpack = require('webpack')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
@@ -31,6 +31,9 @@ let config = {
     'list': ['./src/page/list/index.js'],
     'detail': ['./src/page/detail/index.js'],
     'cart': ['./src/page/cart/index.js'],
+    'order-confirm': ['./src/page/order-confirm/index.js'],
+    'order-list': ['./src/page/order-list/index.js'],
+    'order-detail': ['./src/page/order-detail/index.js'],
     'user-login': ['./src/page/user-login/index.js'],
     'user-register': ['./src/page/user-register/index.js'],
     'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
@@ -77,6 +80,9 @@ let config = {
     new HtmlWebpackPlugin(getHtmlConfig('list', '商品列表')),
     new HtmlWebpackPlugin(getHtmlConfig('detail', '商品详情')),
     new HtmlWebpackPlugin(getHtmlConfig('cart', '购物车')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-confirm', '订单确认')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-list', '订单列表')),
+    new HtmlWebpackPlugin(getHtmlConfig('order-detail', '订单详情')),
     new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
     new HtmlWebpackPlugin(getHtmlConfig('user-register', '用户注册')),
     new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset', '找回密码')),
